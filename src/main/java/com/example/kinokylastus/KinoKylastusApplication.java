5cf5d27bfdadd95d02cf8796006f0e2d9ab4aafe
MattiasKimst <mattias.kimst@ut.ee>
2024-03-08 21:43:21 +0200

    initialize spring boot project

---
+++ src/main/java/com/example/kinokylastus/KinoKylastusApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinokylastus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoKylastusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoKylastusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kinokylastus/KinoKylastusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinokylastus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoKylastusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
